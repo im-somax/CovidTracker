b0a76fb483431419f3275918514b9be2f9341925
Somesh Patel <56200611+im-somax@users.noreply.github.com>
2020-08-22 17:25:27 +0530

    Basic Update donegit add .

--- app/src/main/java/com/example/covidtracker/MainActivity.kt
+++ app/src/main/java/com/example/covidtracker/MainActivity.kt
@@ -3,74 +3,120 @@ package com.example.covidtracker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.AbsListView
+import androidx.constraintlayout.widget.Constraints
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.TimeUnit
 
+
 class MainActivity : AppCompatActivity() {
 
-    lateinit var stateAdapter: StateAdapter
+    lateinit var stateListAdapter: StateListAdapter
 
+    @InternalCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        list.addHeaderView(LayoutInflater.from(this).inflate((R.layout.item_header,list,false)))
+        list.addHeaderView(LayoutInflater.from(this).inflate(R.layout.list_header, list, false))
+
         fetchResults()
+        swipeToRefresh.setOnRefreshListener {
+            fetchResults()
+        }
+        initWorker()
+        list.setOnScrollListener(object : AbsListView.OnScrollListener {
+            override fun onScrollStateChanged(view: AbsListView, scrollState: Int) {}
+            override fun onScroll(
+                view: AbsListView,
+                firstVisibleItem: Int,
+                visibleItemCount: Int,
+                totalItemCount: Int
+            ) {
+                if (list.getChildAt(0) != null) {
+                    swipeToRefresh.isEnabled = list.firstVisiblePosition === 0 && list.getChildAt(
+                        0
+                    ).getTop() === 0
+                }
+            }
+        })
     }
 
     private fun fetchResults() {
         GlobalScope.launch {
-            val response = withContext(Dispatchers.IO) { Client.api.execute() }
+            val response = withContext(Dispatchers.IO) { Client.api.clone().execute() }
             if (response.isSuccessful) {
+                swipeToRefresh.isRefreshing = false
                 val data = Gson().fromJson(response.body?.string(), Response::class.java)
                 launch(Dispatchers.Main) {
                     bindCombinedData(data.statewise[0])
-                    bindStateWiseData(data.statewise.subList(1,data.statewise.size))
+                    bindStateWiseData(data.statewise.subList(0, data.statewise.size))
                 }
             }
         }
     }
 
     private fun bindStateWiseData(subList: List<StatewiseItem>) {
-        stateAdapter = StateAdapter(subList)
-        list.adapter = stateAdapter
+        stateListAdapter = StateListAdapter(subList)
+        list.adapter = stateListAdapter
     }
 
     private fun bindCombinedData(data: StatewiseItem) {
         val lastUpdatedTime = data.lastupdatedtime
         val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
-        lastUpdatedTv.text = "Last Updated\n ${getTimeAgo(simpleDateFormat.parse(lastUpdatedTime))}"
+        lastUpdatedTv.text = "Last Updated\n ${getTimeAgo(
+            simpleDateFormat.parse(lastUpdatedTime)
+        )}"
+
         confirmedTv.text = data.confirmed
-        recoveredTv.text = data.recovered
         activeTv.text = data.active
+        recoveredTv.text = data.recovered
         deceasedTv.text = data.deaths
+
     }
 
-    fun getTimeAgo(past: Date): String {
-        val now = Date()
-        val seconds = TimeUnit.MILLISECONDS.toSeconds(now.time - past.time)
-        val minutes = TimeUnit.MILLISECONDS.toMinutes(now.time - past.time)
-        val hours = TimeUnit.MILLISECONDS.toHours(now.time - past.time)
+    @InternalCoroutinesApi
+    private fun initWorker() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
 
-        return when {
-            seconds < 60 -> {
-                "Few seconds ago"
-            }
-            minutes < 60 -> {
-                "$minutes minutes ago"
-            }
-            hours < 24 -> {
-                "$hours hour ${minutes % 60} min ago"
-            }
-            else -> {
-                SimpleDateFormat("dd/MM/yy, hh:mm a").format(past).toString()
-            }
+        val notificationWorkRequest =
+            PeriodicWorkRequestBuilder<NotificationWorker>(1, TimeUnit.HOURS)
+                .setConstraints(constraints)
+                .build()
+
+        WorkManager.getInstance(applicationContext).enqueueUniquePeriodicWork(
+            "JOB_TAG",
+            ExistingPeriodicWorkPolicy.KEEP,
+            notificationWorkRequest
+        )
+    }
+
+
+}
+
+fun getTimeAgo(past: Date): String {
+    val now = Date()
+    val seconds = TimeUnit.MILLISECONDS.toSeconds(now.time - past.time)
+    val minutes = TimeUnit.MILLISECONDS.toMinutes(now.time - past.time)
+    val hours = TimeUnit.MILLISECONDS.toHours(now.time - past.time)
+
+    return when {
+        seconds < 60 -> {
+            "Few seconds ago"
+        }
+        minutes < 60 -> {
+            "$minutes minutes ago"
+        }
+        hours < 24 -> {
+            "$hours hour ${minutes % 60} min ago"
+        }
+        else -> {
+            SimpleDateFormat("dd/MM/yy, hh:mm a").format(past).toString()
         }
     }
 }
